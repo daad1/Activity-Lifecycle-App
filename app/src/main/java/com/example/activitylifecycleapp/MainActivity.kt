59e8c014745cb61e30e3727c0e651bcc90840241
daad1 <daadrasheid@gmail.com>
2021-11-28 20:42:38 +0300

    Logcat

--- app/src/main/java/com/example/activitylifecycleapp/MainActivity.kt
+++ app/src/main/java/com/example/activitylifecycleapp/MainActivity.kt
@@ -2,38 +2,39 @@ package com.example.activitylifecycleapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-       print("onCreate")
+        Log.v("Test","onCreate")
     }
 
     override fun onStart() {
         super.onStart()
-        print("onStart")
+        Log.v("Test","onStart")
 
     }
 
     override fun onResume() {
         super.onResume()
-        print("onResume")
+        Log.v("Test","onResume")
     }
 
     override fun onPause() {
         super.onPause()
-        print("onPause")
+        Log.v("Test","onPause")
     }
 
     override fun onStop() {
         super.onStop()
-        print("onStop")
+        Log.v("Test","onStop")
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        print("onDestroy")
+        Log.v("Test","onDestroy")
     }
 }
